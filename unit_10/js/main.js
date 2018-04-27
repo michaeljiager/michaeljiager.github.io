5e2b29ff2b7ad1a7e51581412484d09dc09071c4
MichaelGer <michaeljiager@github.com>
2018-04-27 11:04:38 -0400

    Unit 10 Repository

--- unit_10/js/main.js
+++ unit_10/js/main.js
@@ -1,7 +1,22 @@
 jQuery(document).ready(function() {
 
-	var navOffSet = jQuery("nav").Offset().top
-	alert (navOffSet)
+	var navOffSet = jQuery("nav").offset().top
+
+	jQuery("nav").wrap('<div class="nav-placeholder"></div>');
+	jQuery (".nav-placeholder").height(jQuery("nav").outerHeight())
+
+	jQuery(window).scroll(function() {
+		var scrollPos = jQuery(window).scrollTop();
+		
+		if (scrollPos >= navOffset) {
+			jQuery("nav").addClass("fixed");
+		} else {
+			jQuery("nav").removeClass("fixed");
+		}
+
+
+	});
+
 
 
 });
